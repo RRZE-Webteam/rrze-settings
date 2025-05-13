401e9492cd9f96f8316015455044db7addc4ad0e
Rolf <rolf.v.d.forst@fau.de>
2025-05-13 17:16:24 +0200

    Update rrze-settings.php

--- rrze-settings.php
+++ rrze-settings.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name:        RRZE Settings
 Plugin URI:         https://gitlab.rrze.fau.de/rrze-webteam/rrze-settings
-Version:            2.0.0
+Version:            2.0.1
 Description:        General settings and enhancements for a WordPress multisite installation.
 Author:             RRZE Webteam
 Author URI:         https://blogs.fau.de/webworking/
@@ -11,7 +11,7 @@ License:            GNU General Public License Version 3
 License URI:        https://www.gnu.org/licenses/gpl-3.0.html
 Text Domain:        rrze-settings
 Domain Path:        /languages
-Requires at least:  6.7
+Requires at least:  6.8
 Requires PHP:       8.2
 Network:            true
 */
