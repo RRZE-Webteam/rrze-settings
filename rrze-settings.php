f7f771aa6e62fe94f5c4485fcb41e1f8236c5be4
Rolf <rolf.v.d.forst@fau.de>
2025-08-04 14:00:52 +0200

    Update rrze-settings.php

--- rrze-settings.php
+++ rrze-settings.php
@@ -20,26 +20,34 @@ namespace RRZE\Settings;
 
 defined('ABSPATH') || exit;
 
+use RRZE\Settings\Main;
+use RRZE\Settings\Plugin;
 use RRZE\Settings\Plugins\WSForm;
 
-// Composer autoloading
+// Filter to prevent the 'doing_it_wrong' error for the '_load_textdomain_just_in_time' function.
+// This filter checks if the function is called during the 'plugins_loaded' action or if it has already been called.
+add_filter('doing_it_wrong_trigger_error', function ($trigger, $function) {
+    if ($function === '_load_textdomain_just_in_time') {
+        if (doing_action('plugins_loaded') || did_action('plugins_loaded')) {
+            return false;
+        }
+    }
+    return $trigger;
+}, 10, 2);
+
+// Composer autoloading.
 require_once 'vendor/autoload.php';
 
-// Set the WS Form plugin license key if available
+// Set the WS Form plugin license key if available.
 WSForm::setWSFormLicenseKey();
 
-// Register activation hook for the plugin
+// Register activation hook for the plugin.
 register_activation_hook(__FILE__, __NAMESPACE__ . '\activation');
 
-// Register deactivation hook for the plugin
+// Register deactivation hook for the plugin.
 register_deactivation_hook(__FILE__, __NAMESPACE__ . '\deactivation');
 
-/**
- * Add an action hook for the 'plugins_loaded' hook
- *
- * This code hooks into the 'plugins_loaded' action hook to execute a callback function when
- * WordPress has fully loaded all active plugins and the theme's functions.php file.
- */
+// Add an action hook for the 'plugins_loaded' hook.
 add_action('plugins_loaded', __NAMESPACE__ . '\loaded');
 
 /**
@@ -52,11 +60,11 @@ add_action('plugins_loaded', __NAMESPACE__ . '\loaded');
  */
 function activation($networkWide)
 {
-    //
+    // Nothing to do here.
 }
 
 /**
- * Handle the deactivation of the plugin
+ * Handle the deactivation of the plugin.
  *
  * This function is called when the plugin is deactivated.
  * 
@@ -64,18 +72,18 @@ function activation($networkWide)
  */
 function deactivation()
 {
-    // Flush rewrite rules
+    // Flush rewrite rules.
     flush_rewrite_rules();
 }
 
 /**
- * Singleton pattern for initializing and accessing the main plugin instance
+ * Singleton pattern for initializing and accessing the main plugin instance.
  *
  * This method ensures that only one instance of the Plugin class is created and returned.
  *
  * @return Plugin The main instance of the Plugin class
  */
-function plugin()
+function plugin(): Plugin
 {
     // Declare a static variable to hold the instance
     static $instance;
@@ -105,6 +113,49 @@ function loadTextdomain()
 }
 
 /**
+ * Check system requirements for the plugin.
+ *
+ * This method checks if the server environment meets the minimum WordPress and PHP version requirements
+ * for the plugin to function properly.
+ *
+ * @return string An error message string if requirements are not met, or an empty string if requirements are satisfied.
+ */
+function systemRequirements(): string
+{
+    // Get the global WordPress version.
+    global $wp_version;
+
+    // Get the PHP version.
+    $phpVersion = phpversion();
+
+    // Initialize an error message string.
+    $error = '';
+
+    // Check if the WordPress version is compatible with the plugin's requirement.
+    if (!is_wp_version_compatible(plugin()->getRequiresWP())) {
+        $error = sprintf(
+            /* translators: 1: Server WordPress version number, 2: Required WordPress version number. */
+            __('The server is running WordPress version %1$s. The plugin requires at least WordPress version %2$s.', 'rrze-settings'),
+            $wp_version,
+            plugin()->getRequiresWP()
+        );
+    } elseif (!is_php_version_compatible(plugin()->getRequiresPHP())) {
+        // Check if the PHP version is compatible with the plugin's requirement.
+        $error = sprintf(
+            /* translators: 1: Server PHP version number, 2: Required PHP version number. */
+            __('The server is running PHP version %1$s. The plugin requires at least PHP version %2$s.', 'rrze-settings'),
+            $phpVersion,
+            plugin()->getRequiresPHP()
+        );
+    } elseif (!is_multisite() || !is_plugin_active_for_network(plugin()->getBaseName())) {
+        $error = __('The plugin can only be installed in a multisite installation and network-wide.', 'rrze-settings');
+    }
+
+    // Return the error message string, which will be empty if requirements are satisfied.
+    return $error;
+}
+
+/**
  * Handle the loading of the plugin
  *
  * This function is responsible for initializing the plugin, loading text domains for localization,
@@ -114,58 +165,30 @@ function loadTextdomain()
  */
 function loaded()
 {
+    // Load the plugin text domain for translations.
+    loadTextDomain();
+
     // Trigger the 'loaded' method of the main plugin instance.
     plugin()->loaded();
 
-    // Load the plugin textdomain for translations.
-    add_action(
-        'init',
-        __NAMESPACE__ . '\loadTextdomain'
-    );
-
     // Check system requirements.
-    if (
-        ! $wpCompatibe = is_wp_version_compatible(plugin()->getRequiresWP())
-            || ! $phpCompatible = is_php_version_compatible(plugin()->getRequiresPHP())
-                || ! $isMultisite = is_multisite()
-                    || ! $isPluginActiveForNetwork = is_plugin_active_for_network(plugin()->getBaseName())
-    ) {
-        // If the system requirements are not met, add an action to display an admin notice.
-        add_action('init', function () use ($wpCompatibe, $phpCompatible, $isMultisite, $isPluginActiveForNetwork) {
+    if ($error = systemRequirements()) {
+        // If there is an error, add an action to display an admin notice with the error message.
+        add_action('admin_init', function () use ($error) {
             // Check if the current user has the capability to activate plugins.
             if (current_user_can('activate_plugins')) {
-                // Determine the appropriate admin notice tag based on whether the plugin is network activated.
-                $hookName = is_plugin_active_for_network(plugin()->getBaseName()) ? 'network_admin_notices' : 'admin_notices';
-
-                // Get the plugin name for display in the admin notice.
+                // Get plugin data to retrieve the plugin's name.
                 $pluginName = plugin()->getName();
 
-                $error = '';
-                if (! $wpCompatibe) {
-                    $error = sprintf(
-                        /* translators: 1: Server WordPress version number, 2: Required WordPress version number. */
-                        __('The server is running WordPress version %1$s. The plugin requires at least WordPress version %2$s.', 'rrze-settings'),
-                        wp_get_wp_version(),
-                        plugin()->getRequiresWP()
-                    );
-                } elseif (! $phpCompatible) {
-                    $error = sprintf(
-                        /* translators: 1: Server PHP version number, 2: Required PHP version number. */
-                        __('The server is running PHP version %1$s. The plugin requires at least PHP version %2$s.', 'rrze-settings'),
-                        PHP_VERSION,
-                        plugin()->getRequiresPHP()
-                    );
-                } elseif (! $isMultisite || ! $isPluginActiveForNetwork) {
-                    $error = __('The plugin can only be installed in a multisite installation and network-wide.', 'rrze-settings');
-                }
+                // Determine the admin notice tag based on network-wide activation.
+                $tag = is_plugin_active_for_network(plugin()->getBaseName()) ? 'network_admin_notices' : 'admin_notices';
 
-                // Display the error notice in the admin area.
-                // This will show a notice with the plugin name and the error message.
-                add_action($hookName, function () use ($pluginName, $error) {
+                // Add an action to display the admin notice.
+                add_action($tag, function () use ($pluginName, $error) {
                     printf(
                         '<div class="notice notice-error"><p>' .
                             /* translators: 1: The plugin name, 2: The error string. */
-                            esc_html__('Plugins: %1$s: %2$s', 'rrze-settings') .
+                            esc_html__('Plugins: %1$s: %2$s', 'rrze-idp-settings') .
                             '</p></div>',
                         $pluginName,
                         $error
@@ -174,10 +197,10 @@ function loaded()
             }
         });
 
-        // If the system requirements are not met, the plugin initialization will not proceed.
+        // Return to prevent further initialization if there is an error.
         return;
     }
 
-    // If there are no errors, create an instance of the 'Main' class and trigger its 'loaded' method
+    // If there are no errors, create an instance of the 'Main' class and trigger its 'loaded' method.
     (new Main)->loaded();
 }
