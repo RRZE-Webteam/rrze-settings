3b1f243bc5965f261edc699412c0ab04be05096d
Rolf <rolf.v.d.forst@fau.de>
2025-09-23 14:30:22 +0200

    Update rrze-settings.php

--- rrze-settings.php
+++ rrze-settings.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name:        RRZE Settings
 Plugin URI:         https://gitlab.rrze.fau.de/rrze-webteam/rrze-settings
-Version:            2.0.13
+Version:            2.0.14
 Description:        General settings and enhancements for a WordPress multisite installation.
 Author:             RRZE Webteam
 Author URI:         https://blogs.fau.de/webworking/
