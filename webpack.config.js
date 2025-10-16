c16e4091170b8fd4a824bd9cb6a55ef0ee797500
Rolf <rolf.v.d.forst@fau.de>
2025-10-16 17:10:29 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -11,13 +11,15 @@ module.exports = {
     entry: {
         "mail/admin-email": "./src/mail/admin-email.js",
         "media/columns": "./src/media/columns.js",
-        "media/replace": "./src/media/replace.js",
         "media/svg-edit-post": "./src/media/svg-edit-post.js",
         "media/svg": "./src/media/svg",
+        "taxonomies/attachment-media-filters":
+            "./src/taxonomies/attachment-media-filters.js",        
         "menus/expand-collapse": "./src/menus/expand-collapse.js",
         "admin/admin": "./src/admin/admin.js",
         "writing/block-editor": "./src/writing/block-editor.js",
-        "writing/block-editor-preferences": "./src/writing/block-editor-preferences.js",
+        "writing/block-editor-preferences":
+            "./src/writing/block-editor-preferences.js",
         "discussion/avatars": "./src/discussion/avatars.js",
         "advanced/placeholder": "./src/advanced/placeholder.js",
     },
